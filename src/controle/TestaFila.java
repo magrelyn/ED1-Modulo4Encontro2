efb1f27488bccd01c5f94c15733ccd67c77b765b
Magrelin San <magrelinsan@gmail.com>
2020-08-26 12:25:37 -0300

    Versao com reposicionamento no vetor

---
+++ src/controle/TestaFila.java
@@ -0,0 +1,42 @@
+package controle;
+
+import modelo.Fila_Caminhoneiros;
+
+public class TestaFila {
+
+	public static void main(String[] args) {
+
+		Fila_Caminhoneiros fila = new Fila_Caminhoneiros();
+
+		fila.inicializa();
+		
+		System.out.println("#### enfileirar ####\n");
+
+		fila.enfileirar("Luiz Augusto");
+		fila.enfileirar("Bino");
+		fila.enfileirar("Pedro");
+		fila.enfileirar("Jorge");
+		fila.enfileirar("João");
+		fila.enfileirar("Jorge");
+		fila.enfileirar("Grilo");
+		fila.enfileirar("Jão");
+		fila.enfileirar("Marcio");
+		fila.enfileirar("João Vitor");
+		fila.enfileirar("Marcos");
+
+		System.out.println("\n#### mostrarFila ####\n");
+		
+		fila.mostrarFila();
+		
+		System.out.println("\n#### desinfileirar ####\n");
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(fila.desinfileirar());
+		}
+
+		System.out.println(fila.desinfileirar());
+		
+
+	}
+
+}
---
+++ src/modelo/Fila_Caminhoneiros.java
@@ -0,0 +1,58 @@
+package modelo;
+
+public class Fila_Caminhoneiros {
+
+	private String caminhoneiros[] = new String[10];
+	private int total;
+
+	public void inicializa() {
+		this.total = 0;
+	}
+
+	public boolean estaVazia() {
+		if (this.total == 0)
+			return true;
+		else
+			return false;
+	}
+
+	public boolean estaCheia() {
+		if (this.total >= 10)
+			return true;
+		else
+			return false;
+	}
+
+	public void enfileirar(String caminhoneiro) {
+		if (this.estaCheia() == false) {
+			this.total++;
+			this.caminhoneiros[this.total - 1] = caminhoneiro;
+		} else {
+			System.out.println("[!] Fila cheia [!]");
+		}
+	}
+
+	public String desinfileirar() {
+		if (this.estaVazia())
+			return "[!] Fila vazia [!]";
+		else {
+			String resp = this.caminhoneiros[0];
+			this.total--;
+			this.reposiciona();
+			return resp;
+		}
+	}
+	
+	private void reposiciona() {
+		for (int i = 0; i < this.total; i++) {
+			this.caminhoneiros[i] = this.caminhoneiros[i+1];
+		}
+	}
+	
+	public void mostrarFila() {
+		for (int i = 0; i < this.total; i++) {
+			System.out.println(this.caminhoneiros[i]);
+		}
+	}
+
+}
